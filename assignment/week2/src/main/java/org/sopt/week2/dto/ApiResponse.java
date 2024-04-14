c7bf188d0881afed1248a7d75f89c6c3027dd9c1
kgy1008 <gayeon1008@g.skku.edu>
2024-04-14 18:51:25 +0900

    feat: Seminar 코드

---
+++ assignment/week2/src/main/java/org/sopt/week2/dto/ApiResponse.java
@@ -0,0 +1,13 @@
+package org.sopt.week2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class ApiResponse {
+    private String content;
+    public static ApiResponse create(String content){
+        return new ApiResponse(content);
+    }
+}
---
+++ assignment/week2/src/main/java/org/sopt/week2/service/dto/MemberFindDto.java
@@ -0,0 +1,14 @@
+package org.sopt.week2.service.dto;
+
+import org.sopt.week2.domain.Member;
+import org.sopt.week2.domain.Part;
+
+public record MemberFindDto(
+        String name,
+        Part part,
+        int age
+) {
+    public static MemberFindDto of(Member member) {
+        return new MemberFindDto(member.getName(), member.getPart(), member.getAge());
+    }
+}
