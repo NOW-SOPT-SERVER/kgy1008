ef30877d56b792f37822b0c41b39667d285613a5
kgy1008 <gayeon1008@g.skku.edu>
2024-05-01 15:06:51 +0900

    feat: request body validation 진행

--- assignment/week3/src/main/java/org/sopt/week3/service/dto/PostCreateRequest.java
+++ assignment/week3/src/main/java/org/sopt/week3/service/dto/PostCreateRequest.java
@@ -1,7 +1,13 @@
 package org.sopt.week3.service.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record PostCreateRequest(
+        @NotNull(message = "포스트 제목을 입력해주세요")
+        @Size(min = 1, max = 20, message="블로그 제목의 글자 수는 최소 1자에서 최대 20자 사이이여야 합니다.")
         String name,
+        @NotNull(message = "포스트 내용을 입력해주세요")
         String content
 ) {
 }
