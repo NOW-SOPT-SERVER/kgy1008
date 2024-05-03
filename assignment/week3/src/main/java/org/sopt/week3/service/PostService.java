afb874325fb3b5858fe469dafc7cc08ecccd2eeb
kgy1008 <gayeon1008@g.skku.edu>
2024-05-03 14:07:39 +0900

    refactor: 코드 리뷰 반영

--- assignment/week3/src/main/java/org/sopt/week3/service/PostService.java
+++ assignment/week3/src/main/java/org/sopt/week3/service/PostService.java
@@ -11,6 +11,7 @@ import org.sopt.week3.service.dto.PostCreateRequest;
 import lombok.RequiredArgsConstructor;
 import org.sopt.week3.service.dto.PostFindDto;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,16 +23,24 @@ public class PostService {
     private final BlogService blogService;
     private final PostRepository postRepository;
 
+    @Transactional
     public String create(Long memberId, Long blogId, PostCreateRequest postCreateRequest) {
         Member member = memberService.findById(memberId);
         Blog blog = blogService.findById(blogId);
 
-        if (!blog.getMember().equals(member)){
+        if (!checkAuthor(blog, member)) {
             throw new UnauthorizedAccessException(ErrorMessage.MEMBER_NOT_MATCH);
         }
 
         Post post = postRepository.save(Post.create(blog, postCreateRequest));
         return post.getId().toString();
+    }
+
+    private boolean checkAuthor(Blog blog, Member member) {
+        if (blog.getMember().equals(member)) {
+            return true;
+        }
+        return false;
     }
 
     public PostFindDto findPostById(Long postId) {
@@ -42,6 +51,6 @@ public class PostService {
 
     public List<PostFindDto> findPostsByBlogId(Long blogId) {
         List<Post> posts = postRepository.findByBlogId(blogId);
-        return posts.stream().map(PostFindDto::of).collect(Collectors.toList());
+        return posts.stream().map(PostFindDto::of).toList();
     }
 }
