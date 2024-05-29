8d271c0231236437b0daf375aedfd7b40ffb32fa
kgy1008 <gayeon1008@g.skku.edu>
2024-05-29 21:42:38 +0900

    [feat] : access token 재발급하는 로직 구현

---
+++ assignment/week6/src/main/java/org/sopt/week6/service/TokenService.java
@@ -0,0 +1,51 @@
+package org.sopt.week6.service;
+
+import lombok.RequiredArgsConstructor;
+import org.sopt.week6.auth.UserAuthentication;
+import org.sopt.week6.common.dto.ErrorMessage;
+import org.sopt.week6.common.jwt.JwtTokenProvider;
+import org.sopt.week6.domain.Member;
+import org.sopt.week6.domain.redis.Token;
+import org.sopt.week6.exception.NotFoundException;
+import org.sopt.week6.exception.UnauthorizedException;
+import org.sopt.week6.repository.MemberRepository;
+import org.sopt.week6.repository.redis.TokenRepository;
+import org.sopt.week6.service.dto.UserJoinResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final MemberRepository memberRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final TokenRepository tokenRepository;
+
+    @Transactional
+    public UserJoinResponse regenerateAccessToken(
+            String refreshToken,
+            Long userId
+    ) {
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.MEMBER_NOT_FOUND));
+
+        Token token = tokenRepository.findByRefreshToken(refreshToken);
+
+        System.out.println(token.getRefreshToken());
+        System.out.println(refreshToken);
+
+        if (refreshToken.equals(token.getRefreshToken())) {
+            String accessToken = generateAccessToken(member.getId());
+            return UserJoinResponse.of(accessToken, refreshToken, member.getId());
+        }
+
+        throw new UnauthorizedException(ErrorMessage.JWT_UNAUTHORIZED_EXCEPTION);
+    }
+
+    private String generateAccessToken(Long memberId) {
+        return jwtTokenProvider.issueAccessToken(
+                UserAuthentication.createUserAuthentication(memberId)
+        );
+    }
+}
