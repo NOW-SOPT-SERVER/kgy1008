b99733a87ba7a9c2f7a3caa90e67e5bc5f3a7770
kgy1008 <gayeon1008@g.skku.edu>
2024-05-29 21:58:59 +0900

    Delete assignment/week3 directory

--- assignment/week3/src/main/java/org/sopt/week3/exception/UnauthorizedAccessException.java
+++
@@ -1,9 +0,0 @@
-package org.sopt.week3.exception;
-
-import org.sopt.week3.common.dto.ErrorMessage;
-
-public class UnauthorizedAccessException extends BusinessException{
-    public UnauthorizedAccessException(ErrorMessage errorMessage){
-        super(errorMessage);
-    }
-}
