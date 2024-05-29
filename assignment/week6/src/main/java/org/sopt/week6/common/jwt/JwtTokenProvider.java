51c06985b17829dbdc9411e6849871e9960f10e8
kgy1008 <gayeon1008@g.skku.edu>
2024-05-29 17:05:50 +0900

    [feat] : refresh Token 발급 로직 구현

--- assignment/week6/src/main/java/org/sopt/week6/common/jwt/JwtTokenProvider.java
+++ assignment/week6/src/main/java/org/sopt/week6/common/jwt/JwtTokenProvider.java
@@ -17,16 +17,19 @@ public class JwtTokenProvider {
 
     private static final String USER_ID = "userId";
 
-    private static final Long ACCESS_TOKEN_EXPIRATION_TIME = 24 * 60 * 60 * 1000L * 14;
+    private static final Long ACCESS_TOKEN_EXPIRATION_TIME = 24 * 60 * 60 * 1000L;  // 엑서스 토큰 만료 기간
+    private static final Long REFRESH_TOKEN_EXPIRATION_TIME = 24 * 60 * 60 * 1000L * 14;  // 리프레시 토큰 만료 기간
 
     @Value("${jwt.secret}")
     private String JWT_SECRET;
-
 
     public String issueAccessToken(final Authentication authentication) {
         return generateToken(authentication, ACCESS_TOKEN_EXPIRATION_TIME);
     }
 
+    public String issueRefreshToken(final Authentication authentication) {
+        return generateToken(authentication, REFRESH_TOKEN_EXPIRATION_TIME);
+    }
 
     public String generateToken(Authentication authentication, Long tokenExpirationTime) {
         final Date now = new Date();
