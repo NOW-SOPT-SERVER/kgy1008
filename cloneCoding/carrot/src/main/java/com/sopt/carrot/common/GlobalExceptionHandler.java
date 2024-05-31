8fd030e22e3290780de87ae7a3abbb420202ac5b
kgy1008 <gayeon1008@g.skku.edu>
2024-05-31 20:10:17 +0900

    [feat] 예외처리 코드 작성

---
+++ cloneCoding/carrot/src/main/java/com/sopt/carrot/common/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.sopt.carrot.common;
+
+import com.sopt.carrot.common.dto.ErrorResponse;
+import com.sopt.carrot.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(ErrorResponse.of(e.getErrorMessage().getStatus(), e.getMessage()));
+    }
+}
---
+++ cloneCoding/carrot/src/main/java/com/sopt/carrot/common/dto/ErrorMessage.java
@@ -0,0 +1,17 @@
+package com.sopt.carrot.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorMessage {
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "사용자를 찾을 수 없습니다."),
+    REGION_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "지역을 찾을 수 없습니다."),
+    ;
+
+    private final int status;
+    private final String message;
+}
