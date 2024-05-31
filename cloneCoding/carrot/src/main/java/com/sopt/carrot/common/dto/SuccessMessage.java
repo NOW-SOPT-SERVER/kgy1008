50e3c0c8ca21c197b6562e9fec5242a1c9eb5c32
kgy1008 <gayeon1008@g.skku.edu>
2024-05-31 21:27:18 +0900

    [feat] 성공 메세지 추가

--- cloneCoding/carrot/src/main/java/com/sopt/carrot/common/dto/SuccessMessage.java
+++ cloneCoding/carrot/src/main/java/com/sopt/carrot/common/dto/SuccessMessage.java
@@ -10,6 +10,7 @@ public enum SuccessMessage {
 
     PRODUCT_CREATE_SUCCESS("상품 등록 성공"),
     PRODUCT_GET_SUCCESS("지역 별 상품 조회 성공"),
+    HEART_CREATE_SUCCESS("찜하기 성공"),
     ;
 
     private final String message;
