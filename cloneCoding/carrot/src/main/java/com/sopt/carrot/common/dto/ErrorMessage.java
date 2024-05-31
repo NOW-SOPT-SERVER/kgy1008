c8f16d693574aff5e4a178cf71469e0661d2dedf
kgy1008 <gayeon1008@g.skku.edu>
2024-05-31 21:27:28 +0900

    [feat] 예외 메세지 추가

--- cloneCoding/carrot/src/main/java/com/sopt/carrot/common/dto/ErrorMessage.java
+++ cloneCoding/carrot/src/main/java/com/sopt/carrot/common/dto/ErrorMessage.java
@@ -10,6 +10,7 @@ public enum ErrorMessage {
 
     USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "사용자를 찾을 수 없습니다."),
     REGION_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "지역을 찾을 수 없습니다."),
+    PRODUCT_NOT_FOUND(HttpStatus.NOT_FOUND.value(), "상품을 찾을 수 없습니다."),
     ;
 
     private final int status;
